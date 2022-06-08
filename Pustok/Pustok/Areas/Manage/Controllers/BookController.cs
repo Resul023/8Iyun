d849f3db564bd3c61954a6f30b3f37ca03de2271
Resul023 <73m6u7h@code.edu.az>
2022-06-08 02:04:49 -0700

    HomeWork

---
+++ Pustok/Pustok/Areas/Manage/Controllers/BookController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pustok.Areas.Manage.ViewModel;
+using Pustok.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pustok.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class BookController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public BookController(AppDbContext context)
+        {
+            this._context = context;
+        }
+        public IActionResult Index()
+        {
+            BookViewModel bookVW = new BookViewModel
+            {
+                Books = _context.Book.Include(x => x.Author).Include(x=>x.Genre).ToList(),
+
+            };
+            return View(bookVW);
+        }
+    }
+}
---
+++ Pustok/Pustok/Models/Books.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pustok.Models
+{
+    public class Books
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(maximumLength: 60)]
+        public string Name { get; set; }
+        public string Desc { get; set; }
+        public int GenreId { get; set; }
+        public Genres Genre { get; set; }
+        public int AuthorId { get; set; }
+        public Authors Author { get; set; }
+        public  double SalePrice{ get; set; }
+        public double DiscountPercent { get; set; }
+        public int PageSize { get; set; }
+        public double CostPrice { get; set; }
+        public bool IsAvailable { get; set; }
+        public string SubDesc { get; set; }
+        public int Rate { get; set; }
+
+        
+   
+    }
+}
